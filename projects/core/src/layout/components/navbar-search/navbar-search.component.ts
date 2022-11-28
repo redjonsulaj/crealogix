13f896e2cbdb44b575ddffec7351779e9757aab2
mateo <mateo.pishtra@impaladigital.com>
2022-11-28 17:57:51 +0100

    implement history for app

--- projects/core/src/layout/components/navbar-search/navbar-search.component.ts
+++ projects/core/src/layout/components/navbar-search/navbar-search.component.ts
@@ -11,6 +11,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {EntityService} from "../../services/entity.service";
 import {Router} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'lib-navbar-search, [lib-navbar-search]',
@@ -21,7 +22,7 @@ import {Router} from "@angular/router";
 export class NavbarSearchComponent implements OnInit, OnChanges {
   @Input('search') search: any;
   @Input() entity: any;
-  searchHistory: any = [];
+  searchHistory: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
   searchForm: FormGroup = new FormGroup({searchInput: new FormControl(null, {validators: [Validators.required]})});
 
   constructor(private cd: ChangeDetectorRef, private http: HttpClient, public entityService: EntityService, private router: Router) {
@@ -35,18 +36,34 @@ export class NavbarSearchComponent implements OnInit, OnChanges {
   }
 
   private initSearchHistory() {
-    this.searchHistory = Array.from({length: this.search.save}, (v, i) => i);
+    this.searchHistory.next(Array.from({length: this.search.save}, (v, i) => null));
+  }
+   researchItem(val: string) {
+    this.searchForm.controls["searchInput"].setValue(val);
+    this.searchEntity(false);
   }
 
-  searchEntity() {
+  private recordHistory(value: any) {
+    const historyItemIdx = this.searchHistory.value.findIndex((item: string | null) => !item);
+    if (historyItemIdx > -1) {
+      this.searchHistory.value[historyItemIdx] = value;
+      this.searchHistory.next(this.searchHistory.value);
+    }
+  }
+
+
+  searchEntity(newSearch = true) {
     let params = new HttpParams();
-    this.search.entities[this.entity].attributes
-      .forEach((attr: string) => params = params.append(attr, this.searchForm.value['searchInput']));
+    this.search.entities[this.entity].attributes.forEach((attr: string) => params = params.append(attr, this.searchForm.value['searchInput']));
+
     this.http.get(this.search.url, {params}).subscribe((val: any) => {
       const item = val.results[0];
       this.entityService.setItem(item);
       if (!this.entityService.getEntity()) {
         this.entityService.setEntity(this.search.entity);
+      }
+      if (newSearch) {
+        this.recordHistory(this.searchForm.value['searchInput']);
       }
       const url = item.url.split(this.entity)[1].replace(/.$/, '');
       this.router.navigateByUrl(`${this.entity}${url}`, {...item});
